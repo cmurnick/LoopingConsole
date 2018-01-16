3bcba13d0bf4d72ed02072c5982e694bb810df55
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-15 20:50:16 -0600

    added files

---
+++ LoopingConsole/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    class Child
+    {
+        public string Name { get; set; }
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote()
+        {
+            return Sick
+            
+           ? $"Please excuse {Name} from class today becuase they are very sick."
+           : $"{Name}, you still have to go to school.  Quit faking.  Love Mom";
+            
+        }
+    }
+}
--- LoopingConsole/Program.cs
+++ LoopingConsole/Program.cs
@@ -10,6 +10,36 @@ namespace LoopingConsole
     {
         static void Main(string[] args)
         {
+
+            var children = new List<Child>
+            {
+                new Child  { Name = "Suzie", Sick = false },
+                new Child  { Name = "Ralph", Sick = true }
+            };
+
+
+            //for (var i=0; i  <children.Count -1; i++)
+            //{
+            //    var child = children[i];
+            //    Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+            //    Console.WriteLine(child.ForgeASickNote());
+            //}
+
+            foreach (var child in children)
+            {
+                //    if (child.Name == "Suzie")
+                //    {
+                //        continue; //continue the loop and stop executing this iteration of the loop skip this one
+                //break break out of the loop and move on
+                //}
+
+                Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+         
+            Console.WriteLine("This is our second time talking about the same thing.");
+            Console.ReadLine();
         }
     }
 }
